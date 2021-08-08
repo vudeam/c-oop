b8a2d88484bb07a81af2bf2187784e91f7866c38
Vadim Ermakov <vadim22.barnaul@gmail.com>
2021-08-08 20:38:24 +0300

    Add list implementation (extra practice)

--- abstract-data-types/main.c
+++ abstract-data-types/main.c
@@ -1,36 +1,16 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "new.h"
 #include "Object.h"
 #include "Set.h"
-#include "list.h"
-
-
-void list_print(const struct Node * list);
 
 int main(void) {
-	int data = 0xF00DBABA;
-	int data1 = 0xF00DBABA + 1;
-	int data2 = 0xF00DBABA + 2;
-	int data3 = 0xF00DBABA + 3;
-
-	struct Node * list = node_create(&data);
-
-	list_append(&list, &data1);
-	data++;
-	list_append(&list, &data2);
-	data++;
-	list_append(&list, &data3);
-	data++;
-
-	list_print(list);
-
-	/*
 	void * s = new(Set);
 	void * a = add(s, new(Object));
 	void * b = add(s, new(Object));
 	void * c = new(Object);
-	
+
 	if (contains(s, a) && contains(s, b))
 		puts("ok");
 
@@ -45,24 +25,9 @@ int main(void) {
 
 	delete(drop(s, b));
 	delete(drop(s, c));
-	*/
 
 	puts("\n");
 
 	return 0;
-}
-
-void list_print(const struct Node * list) {
-	printf("len=%zu\n", list_length(list));
-	/*for (size_t i = 0; i < list_length(list); i++) {
-		const struct Node * element = list_node_at(list, i);
-		printf("[%zu] %x (%p)", i, *(int *)element, (void *)element);
-	}*/
-	const struct Node * buf = list;
-	for (size_t i = 0;
-			 buf;
-			 buf = buf->next, i++) {
-		printf("[%zu] %x (%p)\n", i, *(int *)buf->data, (void *)buf);
-	}
 }
 
