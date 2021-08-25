47e6766d4789d6f6efd50f9e5c6be4120790e7b6
Vadim Ermakov <vadim22.barnaul@gmail.com>
2021-08-25 12:16:29 +0300

    Create private declarations for Point and Circle

---
+++ 4-inheritance/Circle.r
@@ -0,0 +1,17 @@
+#ifndef CIRCLE_R
+#define CIRCLE_R
+
+#include "Point.r"
+
+
+/**
+ * declaration of Circle - a subclass of a Point
+ */
+struct Circle {
+    const struct Point _;
+    int rad;
+};
+
+
+#endif        /* CIRCLE_R */
+
---
+++ 4-inheritance/Point.r
@@ -0,0 +1,15 @@
+#ifndef POINT_R
+#define POINT_R
+
+
+/**
+ * declaration of Point
+ */
+struct Point {
+    const void * class;
+    int x, y;                  /* coordinates */
+};
+
+
+#endif
+
