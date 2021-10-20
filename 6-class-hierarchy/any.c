007358b5fc543ad4e03d1ebb08e4b0e77cc433af
Vadim Ermakov <vadim22.barnaul@gmail.com>
2021-10-20 11:11:19 +0300

    Add testcase for Any class

---
+++ 6-class-hierarchy/any.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include "Object.h"
+
+
+static int Any_differ (const void * _self, const void * _other) {
+    return 0;        /* Any equals anything... */
+}
+
+int main (void) {
+    void * o = new(Object);
+
+    const void * Any =
+        new(Class, "Any", Object, sizeOf(o),
+                differ, & Any_differ,
+                0);
+
+    void * a = new(Any);
+
+    puto(Any, stdout);
+    puto(o,   stdout);
+    puto(a,   stdout);
+
+    if (differ(o, o) == differ(a, a))
+        puts("ok");
+
+    if (differ(o, a) != differ(a, o))
+        puts("not commutative");
+
+    delete(o), delete(a);
+    delete(Any);
+
+    return 0;
+}
+
